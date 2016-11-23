7f63fc7149b16d8f64fa66cca69f1abee92367fd
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2016-11-23 18:50:37 -0500

    Improved formula

--- R/num2letter.R
+++ R/num2letter.R
@@ -1,18 +1,18 @@
 #' Convert number to equivalent letter
-#' @param x Number to be converted to letter
+#' @param x Number to be converted to letter (only works for numbers less than or equal to 702)
 #' @param number Number to be appended to letter
-#' @return Character string representing Excel column
+#' @return Character string a letter and optional appended number
 #' @export
 #' @description Converts a number to an equivalent letter, with an option to concatenate a row value. Useful for adding data to existing Google Sheets
 
 num2letter <- function(x, number = "") {
   index <- data.frame(Numbers = (1:26), Letters = LETTERS)
-  x.round <- trunc((x-1) / 26)
-
+  x.round <- trunc((x - 1) / 26)
+  
   if (x.round == 0) {
     paste(index[x, 2], number, sep = "")
   } else{
-    if (x.round > 0 & x.round < 26) {
+    if (x.round > 0 & x.round < 27) {
       paste(index[x.round, 2], index[x - x.round * 26, 2], number, sep = "")
     } else {
       print("Input Value too high")
