0907d184a0ec25600361c9db5623c901a24bf249
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-01-23 17:56:19 -0500

    Add corr.summary function

---
+++ R/corr.summary.R
@@ -0,0 +1,19 @@
+#' Clean corr.test output
+#' @param corr.test.results corr.test object
+#' @return Dataframe with clean output
+#' @export
+#' @description Converts corr.test output to single dataframe with most important information
+
+corr.summary <- function(corr.test.results) {
+  cor.df <-
+    lapply(corr.test.results[1:4], function(x) {
+      x %>%
+        as.data.frame() %>%
+        rownames_to_column() %>%
+        gather(key = rowname)
+    }) %>%
+    bind_cols()
+  cor.df <- cor.df[, c(1:3, 6, 9, 12)]
+  names(cor.df) <- c("IV", "DV", "r", "n", "t", "p")
+  mutate(cor.df, Sig = ifelse(p < .05, 1, 0))
+}
--- R/read.xlsx.all.R
+++ R/read.xlsx.all.R
@@ -3,6 +3,8 @@
 #' @param path Path to the xls/xlsx file
 #' @param save2env Either TRUE to save each worksheet to the environment or FALSE to return a list of worksheets, which can be saved to the environment
 #' @param names A vector of character data representing preferred sheet names
+#' @param startRow first row to begin looking for data. Empty rows at the top of a file are always skipped, regardless of the value of startRow.
+#' @param detectDates If TRUE, attempt to recognise dates and perform conversion.
 #' @return If there is more than one worksheet, then a list of data frames that represent each worksheet. If there is only one worksheet, then a data frame.
 #' @export
 
@@ -10,15 +12,23 @@
 read.xlsx.all <-
   function(path,
            save2env = FALSE,
-           names = "") {
+           names = "",
+           startRow = 1,
+           detectDates = TRUE) {
     sheetnames <- openxlsx::getSheetNames(path)
     if (length(sheetnames) == 1) {
-      worksheet <- openxlsx::read.xlsx(path)
+      worksheet <-
+        openxlsx::read.xlsx(path, detectDates, check.names = TRUE, startRow)
     } else{
       make.sheetnames <- make.names(sheetnames, unique = TRUE)
       workbook <-
         pbapply::pblapply(sheetnames, function(x)
-          openxlsx::read.xlsx(path, sheet = x))
+          openxlsx::read.xlsx(
+            path,
+            sheet = x,
+            detectDates,
+            check.names = TRUE
+          ), startRow)
       if (sum(names != "") > 0) {
         names(workbook) <- names
       } else {
