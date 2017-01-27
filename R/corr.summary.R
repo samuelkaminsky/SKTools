27d4e0f2c07d522f6df054714eb8b6c0ae8311f6
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-01-27 17:22:13 -0500

    Fixed corr.summary bug

--- R/corr.summary.R
+++ R/corr.summary.R
@@ -5,14 +5,27 @@
 #' @export
 #' @description Converts corr.test output to single dataframe with most important information (IV, DV, r, n, t, p)
 
-corr.summary <- function(corr.test.results,alpha=.05) {
+corr.summary <- function(corr.test.results, alpha = .05) {
+  # if (!("corr.test" %in% class(corr.test.results))) {
+  # stop("Not a corr.test object")
+  # }
+  if (length(corr.test.results[2] == 1)) {
+    corr.test.results[2] <-
+      list(n = rep(x = corr.test.results[[2]],
+                   reshape2::melt(
+                     tibble::rownames_to_column(as.data.frame(corr.test.results[[1]]))
+                   ) %>% nrow()))
+    
+  }
   cor.df =
     dplyr::bind_cols(lapply(corr.test.results[1:4], function(x) {
       reshape2::melt(tibble::rownames_to_column(as.data.frame(x)))
     }))
+  
   cor.df <- cor.df[, c(1:3, 6, 9, 12)]
   names(cor.df) <- c("IV", "DV", "r", "n", "t", "p")
   cor.df$Sig = ifelse(cor.df$p < alpha, 1, 0)
   cor.df
-
+  
+  
 }
