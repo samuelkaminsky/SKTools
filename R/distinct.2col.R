c503527b99eb7aa8a1a50710c4bed7892c3c97b1
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-04-11 14:49:31 -0400

    Added distinct.2col function

---
+++ R/distinct.2col.R
@@ -0,0 +1,17 @@
+#' Remove duplicates based on two columns, ignoring their order
+#'
+#' @param df Dataframe
+#' @param col1 Quoted name of first column
+#' @param col2 Quoted name of second column
+#' @return Dataframe with duplicates removed
+#' @importFrom dplyr %>% 
+#' @export
+
+distinct.2col <- function(df, col1, col2)
+  df %>%
+  dplyr::mutate(dupevec = purrr::map(1:nrow(.), function(x) {
+    c(.[x, col1] %>% unlist, .[x, col2] %>% unlist) %>% purrr::set_names(c("", "")) %>% sort
+  }),
+  dupevec = as.character(dupevec)) %>%
+  dplyr::distinct(dupevec, .keep_all = TRUE) %>%
+  dplyr::select(-dupevec)
