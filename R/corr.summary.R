0a12fea8c3b6e7fe25a312855a8b079611deecc8
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-04-18 14:37:48 -0400

    Switched from reshape2 to tidyr

--- R/corr.summary.R
+++ R/corr.summary.R
@@ -9,20 +9,32 @@ corr.summary <-
   function(corr.test.results, alpha = .05) {
     if (length(corr.test.results[[2]]) == 1) {
       corr.test.results[2] <-
-        list(n = rep(x = corr.test.results[[2]],
-                     nrow(
-                       reshape2::melt(tibble::rownames_to_column(as.data.frame(
-                         corr.test.results[[1]]
-                       )))
-                     )))
+        list(
+          n = rep(
+            x = corr.test.results[[2]],
+            corr.test.results[[1]] %>%
+              as.data.frame() %>%
+              tibble::rownames_to_column() %>%
+              tidyr::gather(rowname) %>%
+              nrow()
+          )
+        )
     }
     cor.df <-
-      dplyr::bind_cols(lapply(corr.test.results[1:4], function(x) {
-        reshape2::melt(tibble::rownames_to_column(as.data.frame(x)))
-      }
-      ))
-    cor.df <- cor.df[, c(1:3, 6, 9, 12)]
-    names(cor.df) <- c("IV", "DV", "r", "n", "t", "p")
-    cor.df$Sig <-  ifelse(cor.df$p < alpha, 1, 0)
-    cor.df
+      corr.test.results[1:4] %>%
+      purrr::map(~ as.data.frame(.)) %>%
+      purrr::map(~ tibble::rownames_to_column(.)) %>%
+      purrr::map(~ tidyr::gather(., rowname)) %>%
+      dplyr::bind_cols() %>%
+      dplyr::select(
+        IV = rowname,
+        DV = rowname1,
+        r = value,
+        n = value1,
+        t = value2,
+        p = value3
+      ) %>%
+      dplyr::mutate(Sig = dplyr::if_else(.$p < alpha, TRUE, FALSE)) %>%
+      dplyr::filter(!(.$IV == .$DV & .$r == 1))
+    return(cor.df)
   }
