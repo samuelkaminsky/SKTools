a1f6c687b845757273f4dfaef8edf297f18b4b36
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-04-19 15:59:52 -0400

    Removed imports

--- R/qualtrics.prior.distros.R
+++ R/qualtrics.prior.distros.R
@@ -2,48 +2,72 @@
 #' @param surveyId Qualtrics Survey ID
 #' @param header.all Header
 #' @return Data frame of distribution data
-#' @import httr
-#' @importFrom dplyr %>% select mutate distinct left_join
-#' @importFrom purrr map map_df
 #' @description Retrieves distributions for a survey on Qualtrics
 #' @export
 
 qualtrics.prior.distros <-
   function(surveyId, header.all) {
-    distributions.response <-
-      GET(
+    distributions.response <<-
+      httr::GET(
         url = paste0(
           "https://az1.qualtrics.com/API/v3/distributions?surveyId=",
           surveyId
         ),
-        add_headers(header.all)
+        httr::add_headers(header.all)
       ) %>%
-      content()
+      httr::content()
     
-    distributions.list <-
+    distributions.list <<-
       distributions.response$result$elements %>%
-      map(~ .$recipients$mailingListId) %>%
+      purrr::map( ~ .$recipients$mailingListId) %>%
       unlist() %>%
-      map(~ GET(
+      purrr::map( ~ httr::GET(
         url = paste0(
           "https://az1.qualtrics.com/API/v3/mailinglists/",
           .,
           "/contacts"
         ),
-        add_headers(header.all)
+        httr::add_headers(header.all)
       )) %>%
-      map(~ content(.)) %>%
-      map(~ .$result$elements)
+      purrr::map( ~ httr::content(.)) %>%
+      purrr::map( ~ .$result$elements)
     
-    distributions.df <-
-      map_df(1:length(distributions.list), function(x) {
-        map_df(1:length(distributions.list[[x]]), function(y) {
-          distributions.list[[x]][[y]] %>%
-            unlist %>%
-            t %>%
-            as.data.frame() %>%
-            dplyr::mutate_all(as.character)
-        })
-      })
-    return(distributions.df)
+    distributions.df <<-
+      purrr::map_df(1:length(distributions.list), function(x) {
+        purrr::map_df(
+          1:length(distributions.list[[x]]),
+          purrr::possibly(
+            function(y) {
+              distributions.list[[x]][[y]] %>%
+                unlist %>%
+                t() %>%
+                as.data.frame() %>%
+                tibble::repair_names() %>%
+                dplyr::mutate_all(as.character)
+            },
+            otherwise = dplyr::tibble(
+              id = NA_character_,
+              firstName = NA_character_,
+              lastName = NA_character_,
+              email = NA_character_,
+              embeddedData.Position = NA_character_,
+              embeddedData.Candidate.ID = NA_character_,
+              embeddedData.City = NA_character_,
+              embeddedData.Cand.Profile.ID = NA_character_,
+              embeddedData.System.Req.ID = NA_character_,
+              unsubscribed = NA_character_,
+              emailHistory.emailDistributionId = NA_character_,
+              emailHistory.date = NA_character_,
+              emailHistory.type = NA_character_,
+              emailHistory.result = NA_character_,
+              emailHistory.read = NA_character_,
+              emailHistory.surveyId = NA_character_,
+              `embeddedData.Candidate ID` = NA_character_,
+              `embeddedData.Cand Profile ID` = NA_character_
+            )
+          )
+        )
+      }
+      )
+    distributions.df
   }
