37d5c2518b7c0f8813df7a148457c94c97e3a7a5
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-04-24 16:24:18 -0400

    Improved documentation

--- R/install.sk.R
+++ R/install.sk.R
@@ -2,6 +2,34 @@
 #' @param extra TRUE or FALSE to indicate whether you want to install supplemental packages
 #' @export
 #' @description Installs packages that I like
+#' @details Packages installed with the default arguments include: 
+#' * anytime
+#' * data.table
+#' * feather
+#' * knitr
+#' * lavaan
+#' * openxlsx
+#' * psych
+#' * rmarkdown
+#' * roxygen2
+#' * shiny
+#' * sjmisc
+#' * tidyverse
+#' @details Packages installed with the extra=TRUE argument include: 
+#' * car
+#' * caret
+#' * doParallel
+#' * dtplyr
+#' * effsize
+#' * foreach
+#' * GGally
+#' * googlesheets
+#' * gtools
+#' * lsr
+#' * parallel
+#' * semTools
+#' * reshape2
+#' @md
 
 install.sk <- function(extra = FALSE) {
   list.of.packages <-
