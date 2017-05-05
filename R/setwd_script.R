ab307805c5c22f49787e1c8c5c6b4b42bdfa37ec
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-05-04 22:11:39 -0400

    Added setwd_script

---
+++ R/setwd_script.R
@@ -0,0 +1,17 @@
+#' Sets working directory to current script's location
+#' @export
+#' @description Sets working directory to current script's location
+
+setwd_script <- function() {
+  location <- 
+    rstudioapi::getSourceEditorContext()
+  
+  slash <- 
+    stringr::str_locate_all(location$path,"/") %>% 
+    unlist
+  
+  slash.last <- length(slash)
+  
+  wd <- stringr::str_sub(location$path,start = 1,slash[slash.last])
+  setwd(wd)
+}
