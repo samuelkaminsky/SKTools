2a402455be30a5ee9440f0a0970339174c26629b
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-05-10 22:59:23 -0400

    Fix to anova_multi documentation

--- R/anova_multi.R
+++ R/anova_multi.R
@@ -2,7 +2,7 @@
 #' @param df Dataframe with test data
 #' @param iv Name of independent variable
 #' @param dvs Names of dependent variables to be inserted into dplyr::select()
-#' @return List with data frame of tidy t.test results, and Ns and percentages in each level of the IV
+#' @return List with data frame of tidy ANOVA results, and Ns and percentages in each level of the IV
 #' @description Conduct one-way ANOVAs on multipe DVs
 #' @export
 
