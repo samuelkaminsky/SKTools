91bddd5806a909532c56a1f05bc752af2f19d5d6
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-06-18 21:15:41 -0400

    Added test for warning

--- tests/testthat/testnum2letter.R
+++ tests/testthat/testnum2letter.R
@@ -7,3 +7,8 @@ test_that("num2letter works for numbers < 26 and > 26 ", {
   expect_equal(num2letter(26), "Z")
   expect_equal(num2letter(27), "AA")
 })
+
+test_that("num2letter prints warning when number is too high ", {
+  prints_text(num2letter(703), "Input Value too high")
+})
+
