89b0179d997b1b3688f56aa6c4a91f867f9000f9
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-06-26 09:19:45 -0500

    Fix github_update

--- R/github_update.R
+++ R/github_update.R
@@ -3,15 +3,18 @@
 #' @description Updates packages that have been installed from Github
 
 github_update <- function() {
+  check <- "SKTools" %in% .packages()
+  if (check) {
+    detach("package:SKTools", unload = TRUE)
+  }
   utils::installed.packages() %>%
     tibble::as_tibble() %>%
     .$Package %>%
     purrr::set_names()  %>%
     purrr::map(utils::packageDescription) %>%
-    purrr::map( ~ paste0(.$GithubUsername, "/", .$GithubRepo)) %>%
-    purrr::map_if(nchar(.) > 3, purrr::safely( ~ devtools::install_github(.)))
-  if ("SKTools" %in% loadedNamespaces()) {
-    unloadNamespace("SKTools")
-    loadNamespace("SKTools")
+    purrr::map(~ paste0(.$GithubUsername, "/", .$GithubRepo)) %>%
+    purrr::map_if(nchar(.) > 3, purrr::safely(~ devtools::install_github(.)))
+  if (check) {
+    library("SKTools")
   }
 }
