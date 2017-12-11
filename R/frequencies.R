95046578aff1bc926aa878833045bd4ac570842c
samuelkaminsky <samuel.e.kaminsky@gmail.com>
2017-12-11 16:39:52 -0500

    Added percentages argument

--- R/frequencies.R
+++ R/frequencies.R
@@ -1,12 +1,14 @@
 #' Dataframe Frequencies
 #' @param df Dataframe with data
+#' @param perc Logical indicating whether or not to calculate percentages for each value by variable
 #' @return Dataframe of frequencies for each variable and value
 #' @description Calculates frequencies of every Column in a dataframe
 #' @export
 
 frequencies <-
-  function(df) {
-    df %>%
+  function(df, perc = FALSE) {
+    result <- 
+      df %>%
       names() %>%
       purrr::set_names() %>%
       purrr::map_df(function(x) {
@@ -16,4 +18,12 @@ frequencies <-
           purrr::set_names("value", "n") %>%
           dplyr::mutate(value = as.character(.data$value))
       }, .id = "var")
+    
+    if (perc == TRUE) {
+      result <- 
+        result %>% 
+        dplyr::group_by(.data$var) %>% 
+        dplyr::mutate(perc = .data$n/sum(.data$n, na.rm = TRUE))
+    }
+    result
   }
