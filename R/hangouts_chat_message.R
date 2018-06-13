1fde7df71c109bbd2edd025ab021931440753528
samuelkaminsky <samuelkaminsky@samuelkaminsky-macbookpro.roam.corp.google.com>
2018-06-13 13:34:08 -0700

    Fixed apostrophes

--- R/hangouts_chat_message.R
+++ R/hangouts_chat_message.R
@@ -1,6 +1,6 @@
 #' Sends a message to room in Hangouts Chat using a configured webhook
 #' @param url Character string with the full Webhook URL
-#' @param message Character string to be sent to Hangouts Chat, does not accept strings with apostrophes
+#' @param message Character string to be sent to Hangouts Chat
 #' @return POST results
 #' @description Send a message to a room in Hangouts Chat
 #' @export
@@ -8,15 +8,8 @@
 hangouts_chat_message <- function(url, message) {
   header <- httr::add_headers(c('Content-Type' = "application/json",
                                 'charset' = 'UTF-8'))
-  # message <- stringr::str_replace_all(message, "'", "\'")
-  # message <- stringr::str_replace_all(message, '"', '\\"')
-  # message <- stringr::str_replace_all(message, "[\n\r]", ' ')
-  # if (is.null(url)) {
-    stopifnot(is.character(url), is.character(message))
-    # message("No URL")
-    # return(invisible())
-  # }
-  
+  message <- stringr::str_replace_all(message, "'", "\\\\u0027")
+  stopifnot(is.character(url), is.character(message))
   httr::POST(url,
              header,
              body = paste0("{'text' : '", message, "'}"))
