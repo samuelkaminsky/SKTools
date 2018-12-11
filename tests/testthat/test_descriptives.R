64115e5187e6dd6e3f4908314e96f5fd2fdff131
samuelkaminsky <samuelkaminsky@samuelkaminsky-macbookpro.roam.corp.google.com>
2018-12-10 17:23:58 -0800

    Added descriptives test

---
+++ tests/testthat/test_descriptives.R
@@ -0,0 +1,8 @@
+library(SKTools)
+
+context("descriptives")
+
+test_that("descriptives works for mtcars", {
+  expect_equal(descriptives(mtcars) %>% nrow(), 11)
+  expect_equal(descriptives(mtcars) %>% .[1,7] %>% round(3), 0.499)
+})
