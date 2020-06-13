a33ae5acfa07fb650883b12dda76d3d610d7df31
Samuel Kaminsky <samuelkaminsky@Samuels-MacBook-Air.local>
2020-06-12 18:04:26 -0700

    Removed commented code and reformatted

--- R/corr_summary.R
+++ R/corr_summary.R
@@ -1,31 +1,28 @@
 #' Clean corr.test output
 #' @param corr_test_results corr.test object
 #' @param alpha Alpha level to test significance
-#' @return Dataframe with clean output
+#' @return tibble with clean output
 #' @export
-#' @description Converts corr.test output to tidy dataframe with most important information (IV, DV, r, n, t, p)
+#' @description Converts corr.test output to tidy tibble with most important information (IV, DV, r, n, t, p)
 
 corr_summary <-
   function(corr_test_results, alpha = .05) {
     if (length(corr_test_results[[2]]) == 1) {
-      # corr_test_results[2] <-
-      #   list(
-      #     n = rep(
-      #       x = corr_test_results[[2]],
-      #       corr_test_results[[1]] %>%
-      #         as.data.frame() %>%
-      #         tidyr::gather() %>%
-      #         nrow()
-      #     )
-      #   )
-      corr_test_results[[2]] <- matrix(data = corr_test_results[[2]], corr_test_results[[1]] %>% nrow(), corr_test_results[[1]] %>% nrow(), 
-                                       dimnames = list(rownames(corr_test_results[[1]]), colnames(corr_test_results[[1]])))
+      corr_test_results[[2]] <- matrix(
+        data = corr_test_results[[2]],
+        corr_test_results[[1]] %>% nrow(),
+        corr_test_results[[1]] %>% nrow(),
+        dimnames = list(
+          rownames(corr_test_results[[1]]),
+          colnames(corr_test_results[[1]])
+        )
+      )
     }
     cor.df <-
       corr_test_results[1:4] %>%
-      purrr::map(~ as.data.frame(.)) %>%
-      purrr::map(~ tibble::rownames_to_column(., var = "iv")) %>%
-      purrr::map(~ tidyr::gather(., dv, value, -iv)) %>%
+      purrr::map( ~ as.data.frame(.)) %>%
+      purrr::map( ~ tibble::rownames_to_column(., var = "iv")) %>%
+      purrr::map( ~ tidyr::gather(., dv, value,-iv)) %>%
       purrr::reduce(dplyr::left_join, by = c("iv", "dv")) %>%
       dplyr::select(
         .data$iv,
