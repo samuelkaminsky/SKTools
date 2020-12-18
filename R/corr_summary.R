115a3b8cbe9bcbd918a5ce4b98f8480a608d3cfd
Samuel Kaminsky <23510580+samuelkaminsky@users.noreply.github.com>
2020-12-18 09:42:34 -0800

    Update corr_summary.R
    Improve handling of adjusted p values

--- R/corr_summary.R
+++ R/corr_summary.R
@@ -5,34 +5,59 @@
 #' @export
 #' @description Converts corr.test output to tidy tibble with most important information (IV, DV, r, n, t, p)
 
-corr_summary <-
-  function(corr_test_results, alpha = .05) {
-    if (length(corr_test_results$n) == 1) {
-      corr_test_results$n <- matrix(
-        data = corr_test_results$n,
-        nrow(corr_test_results$r),
-        nrow(corr_test_results$r),
-        dimnames = list(
-          rownames(corr_test_results$r),
-          colnames(corr_test_results$r)
+CorrSummary <-
+  function(corr.test.results, alpha = .05) {
+    if (length(corr.test.results$n) == 1L) {
+      corr.test.results$n <-
+        matrix(
+          data = corr.test.results$n,
+          nrow(corr.test.results$r),
+          nrow(corr.test.results$r),
+          dimnames = list(
+            rownames(corr.test.results$r),
+            colnames(corr.test.results$r)
+          )
         )
-      )
     }
+    corr.test.results.sub <- corr.test.results[c("r", "n", "t", "p", "p")]
+    corr.test.results.sub.named <-
+      purrr::set_names(corr.test.results.sub,
+                       c("r", "n", "t", "p", "p.adjust"))
     cor.df <-
-      corr_test_results[1:4] %>%
-      purrr::map( ~ as.data.frame(.)) %>%
-      purrr::map( ~ tibble::rownames_to_column(., var = "iv")) %>%
-      purrr::map( ~ tidyr::gather(., dv, value,-iv)) %>%
-      purrr::reduce(dplyr::left_join, by = c("iv", "dv")) %>%
-      dplyr::select(
-        .data$iv,
-        .data$dv,
-        r = .data$value.x,
-        n = .data$value.y,
-        t = .data$value.x.x,
-        p = .data$value.y.y
+      corr.test.results.sub.named %>%
+      # Replace bottom (unadjusted p values with NA)
+      purrr::map_at("p.adjust", function(x) {
+        x[lower.tri(x)] <- NA_real_
+        x
+      }) %>%
+      # Replace top (adjusted p values with NA)
+      purrr::map_at("p", function(x) {
+        x[upper.tri(x)] <- NA_real_
+        x
+      }) %>%
+      # Convert to tibble and create column with rownames
+      purrr::map( ~ tibble::as_tibble(., rownames = "iv")) %>%
+      # Convert to long format
+      purrr::imap(
+        ~ tidyr::pivot_longer(
+          .x,
+          cols = -.data$iv,
+          names_to = "dv",
+          values_to = .y,
+          # Removes rows with no p values
+          values_drop_na = TRUE
+        )
       ) %>%
-      dplyr::mutate(Sig = .data$p < alpha) %>%
+      # For p value table, stack to create rows with dv/iv pairs
+      purrr::map_at(c("p", "p.adjust"), function(x) {
+        x.rev <-
+          dplyr::rename(x, dv = iv, iv = dv)
+        dplyr::bind_rows(x, x.rev)
+      }) %>%
+      purrr::reduce(dplyr::left_join, by = c("iv", "dv")) %>%
+      # Calculate significance
+      dplyr::mutate_at(dplyr::vars(p, p.adjust), list(sig = ~ . < alpha)) %>%
+      # Remove rows where dv is matched with iv
       dplyr::filter(!(.data$iv == .data$dv & .data$r == 1))
     return(cor.df)
   }
