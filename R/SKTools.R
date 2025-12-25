54ca264314ba3e3184376c005e13900b494688c5
Sam Kaminsky <samuel.e.kaminsky@gmail.com>
2025-12-24 17:11:27 -0800

    Fix R CMD check issues: dependencies, globals, and ignored files

--- R/SKTools.R
+++ R/SKTools.R
@@ -4,4 +4,15 @@
 #'
 #' @importFrom rlang .data
 #' @importFrom rlang :=
+#' @importFrom dplyr everything where across
 "_PACKAGE"
+
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    "percentage",
+    "IV",
+    "DV",
+    "estimate",
+    "estimate1"
+  ))
+}
